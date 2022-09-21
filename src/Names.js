fa815748058d340a444babac24166791fff6b6b5
PhilippSchlechter <110399840+PhilippSchlechter@users.noreply.github.com>
2022-09-21 21:20:12 +0200

    Add 2nd version

---
+++ src/Names.js
@@ -0,0 +1,18 @@
+export default function Names() {
+  const users = [
+    { id: 0, name: 'Rhaisa' },
+    { id: 1, name: 'Sara' },
+    { id: 2, name: 'Sari' },
+    { id: 3, name: 'Tamara' },
+    { id: 4, name: 'Philipp' },
+  ];
+
+  return (
+    <div>
+      Names:
+      {users.map((user) => {
+        return <div key={`user-${user.id}`}>{user.name}</div>;
+      })}
+    </div>
+  );
+}
